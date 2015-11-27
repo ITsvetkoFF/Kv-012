8f86f9595efd320630b31d90bf7629c6ff8542fd
itsvetkoff <tsvetkoff@i.ua>
2015-11-27 15:35:42 +0200

    test sceleton

---
+++ horse-test.js
@@ -0,0 +1,63 @@
+var solutions = {};
+
+solutions.fake_fakovich = function (Board) {
+    function pauseComp(ms) {
+        var curr = new Date().getTime();
+        ms += curr;
+        while (curr < ms) {
+            curr = new Date().getTime();
+        }
+    }
+
+    pauseComp(Math.random() * 2000);
+    return Array(5);
+
+};
+
+
+// YOUR SOLUTION
+solutions.boiko_natalia = function (Board) {
+    // GOES HERE
+};
+
+
+// YOUR SOLUTION
+solutions.dobrooskok_yaroslav = function (Board) {
+    // GOES HERE
+};
+
+
+// YOUR SOLUTION
+solutions.kozynets_oleg = function (Board) {
+    // GOES HERE
+};
+
+
+// YOUR SOLUTION
+solutions.maltsev_valerii = function (Board) {
+    // GOES HERE
+};
+
+
+// YOUR SOLUTION
+solutions.martyniuk_oleksandra = function (Board) {
+    // GOES HERE
+};
+
+
+// YOUR SOLUTION
+solutions.melnykov_andrii = function (Board) {
+    // GOES HERE
+};
+
+
+// YOUR SOLUTION
+solutions.pylhun_valerii = function (Board) {
+    // GOES HERE
+};
+
+
+// YOUR SOLUTION
+solutions.vaskovska_anna = function (Board) {
+    // GOES HERE
+};
