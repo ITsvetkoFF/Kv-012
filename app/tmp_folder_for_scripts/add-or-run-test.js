e11530f1aad2611200aa8a560d0ae92b08b37c86
OlehKSS <alegsmail@mail.ru>
2015-12-03 18:55:29 +0200

    First remote test

--- app/tmp_folder_for_scripts/add-or-run-test.js
+++ app/tmp_folder_for_scripts/add-or-run-test.js
@@ -6,6 +6,10 @@ $(document).onload(
     $("#run-test-bug").on("click", function () {
     $("#run-test-buttons").css({"display": "none"});
     $("#run-test-form").append("Now you will add bug description");
+        var add_btn = $(button);
+        add_btn.attr({"type": "button", "class": "btn btn-primary"});
+        add_btn.text = "Button";
+        $("#run-test-form").append(add_btn);
 }));
 
 //************************************************************************************
