5eb4f7e24ad9a4309ee4f895a06bb2364583005d
OlehKSS <alegsmail@mail.ru>
2015-12-03 18:48:49 +0200

    First remote test

---
+++ app/tmp_folder_for_scripts/add-or-run-test.js
@@ -0,0 +1,13 @@
+/**
+ * Created by okozytc on 03.12.2015.
+ */
+//run-test
+$(document).onload(
+    $("#run-test-bug").on("click", function () {
+    $("#run-test-buttons").css({"display": "none"});
+    $("#run-test-form").append("Now you will add bug description");
+}));
+
+//************************************************************************************
+//************************************************************************************
+//add-test-case
