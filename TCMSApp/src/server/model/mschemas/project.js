1182ee762d9c2f053f0ca1c68bafcf1d3ddfce2f
OlehKSS <alegsmail@mail.ru>
2015-12-24 16:03:43 +0200

    Enhanced models and Schemas

---
+++ TCMSApp/src/server/model/mschemas/project.js
@@ -0,0 +1,17 @@
+// import the necessary modules
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+// create  the project model
+
+    // define schema
+    var projectSchema = new Schema({
+        name: {type: String, required: true},
+        description: {type: String, required: true},
+        users:  [mongoose.Schema.Types.ObjectId],
+        suites: [mongoose.Schema.Types.ObjectId],
+        dateStart: {type: Date, required: true},
+        dateEnd: {type: Date, required: true}
+    });
+    var project = mongoose.model('Project', projectSchema);
+module.exports = project;
---
+++ TCMSApp/src/server/model/mschemas/run.js
@@ -0,0 +1,57 @@
+// import the necessary modules
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+// create  the run model
+
+    // define schema
+    var runSchema = new Schema({
+        previousRunId: mongoose.Schema.Types.ObjectId,
+        dateStart: {type: Date, required: true},
+        dateEnd: {type: Date, required: true},
+        build: Number,
+        environment: String,
+        status: {
+            type: String,
+            validate: {
+                validator: function (v) {
+                    return /passed|executing|failed/.test(v);
+                },
+                message: '{VALUE} is not valid status!'
+            },
+            default: 'executing'
+        },
+        //the
+        tests: [{
+            testName: {type: String, required: true},
+            testDescription: {type: String, required: true},
+            automated: Boolean,
+            preConditions: {type: String, required: true},
+            steps: [{
+                stepNumber: Number,
+                stepDescription: {type: String, required: true},
+                expectedResult: {type: String, required: true},
+
+                status: {
+                    type: String,
+                    validate: {
+                        validator: function (v) {
+                            return /passed, blocked, failed/.test(v);
+                        },
+                        message: '{VALUE} is not valid status!'
+                    }
+                }
+            }],
+            status: {
+                type: String,
+                validate: {
+                    validator: function (v) {
+                        return /passed|executing|failed/.test(v);
+                    },
+                    message: '{VALUE} is not valid status!'
+                }
+            }
+        }]
+    });
+    var run = mongoose.model('Run',runSchema);
+module.exports = run;
