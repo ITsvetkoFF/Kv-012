6095da98a85bea68b8963eeacb60210542683184
OlehKSS <alegsmail@mail.ru>
2016-01-13 17:52:33 +0200

    Changed some code in the models files: added enum validators, several uniques properties and changed linking between files

--- TCMSApp/src/server/model/mschemas/run.js
+++ TCMSApp/src/server/model/mschemas/run.js
@@ -25,45 +25,10 @@ var Schema = mongoose.Schema;
         envFull: {},
         status: {
             type: String,
-            validate: {
-                validator: function (v) {
-                    return /passed|executing|failed/.test(v);
-                },
-                message: '{VALUE} is not valid status!'
-            },
-            default: 'executing'
+            enum: ['passed', 'executing', 'failed', 'none'],
+            default: 'none'
         },
-        //the
-        tests: [{
-            testName: {type: String, required: true},
-            testDescription: {type: String, required: true},
-            automated: Boolean,
-            preConditions: {type: String, required: true},
-            steps: [{
-                stepNumber: Number,
-                stepDescription: {type: String, required: true},
-                expectedResult: {type: String, required: true},
-
-                status: {
-                    type: String,
-                    validate: {
-                        validator: function (v) {
-                            return /passed, blocked, failed/.test(v);
-                        },
-                        message: '{VALUE} is not valid status!'
-                    }
-                }
-            }],
-            status: {
-                type: String,
-                validate: {
-                    validator: function (v) {
-                        return /passed|executing|failed/.test(v);
-                    },
-                    message: '{VALUE} is not valid status!'
-                }
-            }
-        }]
+        tests: [mongoose.Schema.Types.ObjectId]
     });
     var run = mongoose.model('Run',runSchema);
 module.exports = run;
