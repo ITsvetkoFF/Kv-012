e0955ab9428bb85e8be70f7e070187b96697fee8
OlehKSS <alegsmail@mail.ru>
2016-01-14 17:36:37 +0200

    Additition of the new constants

--- TCMSApp/src/client/app/core/constants.js
+++ TCMSApp/src/client/app/core/constants.js
@@ -7,4 +7,12 @@
         .constant('moment', moment)
         .constant('faker', faker)
         .constant('Trello', Trello)
+        .constant({
+                hostURL: 'http://localhost:3000',
+                defectsURL: '/api/v1/Defects',
+                runsURL: '/api/v1/Runs',
+                projectsURL: '/api/v1/Projects',
+                suitesURL: '/api/v1/Suites',
+                usersURL: '	/api/v1/Users'
+        })
 })();
