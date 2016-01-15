4563fff03f42d9cbcd2091e0808ac62fba79663a
OlehKSS <alegsmail@mail.ru>
2016-01-15 18:47:28 +0200

    The temporary variables were added

--- TCMSApp/src/client/app/core/constants.js
+++ TCMSApp/src/client/app/core/constants.js
@@ -6,5 +6,17 @@
         .module('app.core')
         .constant('moment', moment)
         .constant('Trello', Trello)
-        .constant('faker', faker);
+        .constant('faker', faker)
+        .constant('apiUrl', apiUrl());
+
+    function apiUrl() {
+        return {
+            host: 'http://localhost:3000',
+            defects: '/api/v1/Defects',
+            runs: '/api/v1/Runs',
+            projects: '/api/v1/Projects',
+            suites: '/api/v1/Suites',
+            users: '/api/v1/Users'
+        };
+    }
 })();
