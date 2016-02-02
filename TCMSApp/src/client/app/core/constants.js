e38c82f0e4da6a699246e9a7c14f886bcc60a7f2
Ihor Tsvietkov <tsvetkoff@i.ua>
2016-02-02 15:58:26 +0200

    Update constants.js

--- TCMSApp/src/client/app/core/constants.js
+++ TCMSApp/src/client/app/core/constants.js
@@ -17,7 +17,7 @@
             defects: '/api/v1/Defects',
             runs: '/api/v1/Runs',
             projects: '/api/v1/Projects',
-            suites: '/api/v1/Suites/',
+            suites: '/api/v1/Suites',
             users: '/api/v1/Users/:id',
             suiteTests: '/api/v1/suitetests',
             runTests: '/api/v1/runTests'
