f49c687ae8be8827a5b9fd092c95c5cb010921a0
o.martynyuk <liberty_92@inbox.ru>
2016-02-22 19:17:54 +0200

    Now we see projects of current user

--- TCMSApp/src/server/model/mschemas/project.js
+++ TCMSApp/src/server/model/mschemas/project.js
@@ -9,7 +9,7 @@ var projectSchema = new Schema({
         name: {type: String, required: true},
         trelloOrganizationId: {type: String},
         description: {type: String},
-        admins:  [{type: String, required: true}],
+        admins:  [mongoose.Schema.Types.ObjectId],
         users:  [mongoose.Schema.Types.ObjectId],
         suites: [mongoose.Schema.Types.ObjectId],
         dateStart: {type: Date, required: true},
